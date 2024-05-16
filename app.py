e1a9f7c815564ad8a5edd1cbdeeb190a96bea749
Marcello Fidjestøl <mefidjes@stud.ntnu.no>
2024-05-16 17:24:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def execute_python_code(code):
         return str(e)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
